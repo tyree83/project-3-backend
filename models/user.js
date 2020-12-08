6354ec789558ce7abe77196706d9bd46753b5774
Tyree Christensen <tyree83@gmail.com>
2020-12-08 11:22:18 -0500

    initial commit

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: String,
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true
+  },
+  password: String
+}, {
+  timestamps: true
+});
+
+userSchema.set('toJSON', {
+  transform: function (doc, ret) {
+    // remove the password property when serializing doc to JSON
+    delete ret.password;
+    return ret;
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
