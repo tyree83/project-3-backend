91959d7bfc8745e580c1d791ed3a3c181dae6ed5
Tyree Christensen <tyree83@gmail.com>
2020-12-08 11:43:37 -0500

    adds JSON webtoken package add readme with instructions

---
+++ README.md
@@ -0,0 +1,13 @@
+# PROJECT 3
+
+# INSTRUCTIONS
+
+* `npm i`
+* create a `.env` file in the root of the project
+* add the following to the `.env` file:
+
+```
+DATABASE_URL=<database url goes here>/project3?retryWrites=truew&w=majority
+SECRET=<secret goes here>
+
+```
--- models/user.js
+++ models/user.js
@@ -1,25 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const SALT_ROUNDS = 6;
 
-const userSchema = new Schema({
-  name: String,
-  email: {
-    type: String,
-    required: true,
-    lowercase: true,
-    unique: true
+const userSchema = new Schema(
+  {
+    name: String,
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: String,
   },
-  password: String
-}, {
-  timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
-userSchema.set('toJSON', {
+userSchema.set("toJSON", {
   transform: function (doc, ret) {
     // remove the password property when serializing doc to JSON
     delete ret.password;
     return ret;
-  }
+  },
 });
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+  // password has been changed - salt and hash it
+  bcrypt.hash(user.password, SALT_ROUNDS, function (err, hash) {
+    if (err) return next(err);
+    // replace the user provided password with the hash
+    user.password = hash;
+    next();
+  });
+});
+
+module.exports = mongoose.model("User", userSchema);
