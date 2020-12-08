61494bdbc1642c9c6e3875595b2bcebd7627b15f
Tyree Christensen <tyree83@gmail.com>
2020-12-08 11:53:57 -0500

    generates jwt on user signup

--- controllers/users.js
+++ controllers/users.js
@@ -10,10 +10,18 @@ async function signup(req, res) {
   const user = new User(req.body);
   try {
     await user.save();
-    // TODO: Send back a JWT instead of the user
-    res.json(user);
+    const token = createJWT(user);
+    res.json({ token, user });
   } catch (err) {
     // Probably a duplicate email
     res.status(400).json(err);
   }
+}
+
+function createJWT(user) {
+  return jwt.sign(
+    {user}, //data payload
+    SECRET,
+    {expiresIn: '60d'}
+  );
 }
